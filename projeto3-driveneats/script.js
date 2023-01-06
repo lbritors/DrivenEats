bec1184aad7223045e609c2c99a4e6d063b5cfb0
lbritors <britoleticiars@gmail.com>
2023-01-06 12:37:03 -0300

    selection of itens with problems to solve

--- projeto3-driveneats/script.js
+++ projeto3-driveneats/script.js
@@ -1,12 +1,44 @@
+function mostrarCheck() {
+    const mostrar = document.querySelector('.categoria .escondido');
+    mostrar.classList.remove('escondido');
+}
+function esconderCheck() {
+    const esconder = document.querySelector('.categoria .escondido');
+    esconder.classList.add('escondido');
+}
+
 function selecionarPedido(pedidoSelecionado) {
-    const botaoSelecionadoAnteriormente = document.querySelector('.selecionado');
-    if (botaoSelecionadoAnteriormente !== null) {
-        
-        botaoSelecionadoAnteriormente.classList.remove('selecionado');
+    const botaoSelecionadoAnteriormentePrato = document.querySelector('.prato .selecionado');
+   
+    if (botaoSelecionadoAnteriormentePrato !== null) {
+        botaoSelecionadoAnteriormentePrato.classList.remove('selecionado');
+        esconderCheck();
+
+    } else {
+        pedidoSelecionado.classList.add('selecionado');
+        mostrarCheck();
     }
+}
 
 
-    const selecionou = document.querySelector(pedidoSelecionado);
-    selecionou.classList.add('selecionado');
-       
+function selecionarPedido2(pedidoSelecionado2) {
+    const botaoSelecionadoAnteriormenteBebida = document.querySelector('.bebida .selecionado');
+
+    if (botaoSelecionadoAnteriormenteBebida !== null) {
+        botaoSelecionadoAnteriormenteBebida.classList.remove('selecionado');
+    } else {
+        pedidoSelecionado2.classList.add('selecionado');
+    }
+    
+}
+
+function selecionarPedido3(pedidoSelecionado3) {
+    const botaoSelecionadoAnteriormenteDoce = document.querySelector('.doce .selecionado');
+
+    if (botaoSelecionadoAnteriormenteDoce!== null) {
+        botaoSelecionadoAnteriormenteDoce.classList.remove('selecionado');
+    } else {
+        pedidoSelecionado3.classList.add('selecionado');
+    }
+    
 }
